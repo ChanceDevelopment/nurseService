ca5e55897a8dc026f4954a4d571d3f1554bad4a2
kobe1081 <619305847@qq.com>
2016-12-21 21:51:48 +0800

    修改个人中心

--- nurseService/Controller/Home/MineViewController/MineViewController.m
+++ nurseService/Controller/Home/MineViewController/MineViewController.m
@@ -30,16 +30,16 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
-//        UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
-//        label.backgroundColor = [UIColor clearColor];
-//        label.font = APPDEFAULTTITLETEXTFONT;
-//        label.textColor = APPDEFAULTTITLECOLOR;
-//        label.textAlignment = NSTextAlignmentCenter;
-//        self.navigationItem.titleView = label;
-//        label.text = @"我的";
-//        [label sizeToFit];
-//        self.title = @"我的";
-//        self.navigationItem.titleView.hidden = YES;
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+        label.backgroundColor = [UIColor clearColor];
+        label.font = APPDEFAULTTITLETEXTFONT;
+        label.textColor = APPDEFAULTTITLECOLOR;
+        label.textAlignment = NSTextAlignmentCenter;
+        self.navigationItem.titleView = label;
+        label.text = @"我的";
+        [label sizeToFit];
+        self.title = @"我的";
+        self.navigationItem.titleView.hidden = YES;
     }
     return self;
 }
@@ -49,6 +49,18 @@
     // Do any additional setup after loading the view from its nib.
     [self initializaiton];
     [self initView];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:YES];
+    self.navigationController.navigationBarHidden = YES;
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:YES];
+    self.navigationController.navigationBarHidden = NO;
 }
 
 - (void)initializaiton
